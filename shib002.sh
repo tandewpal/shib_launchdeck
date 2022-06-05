6492a38d5e9007dd83397179272273b610aff4d5
tandewpal <106701896+tandewpal@users.noreply.github.com>
2022-06-05 07:16:36 -0500

    removed screen

--- shib002.sh
+++ shib002.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 lscpu
-sudo apt install screen
-screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0/
