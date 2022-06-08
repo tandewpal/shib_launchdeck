66c04bedb066bec3d205ce9733d29d7e303c361a
tandewpal <106701896+tandewpal@users.noreply.github.com>
2022-06-08 01:25:22 -0700

    Update shib001.sh

--- shib001.sh
+++ shib001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 lscpu
-curl https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0/
 ./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0xca3938760d3ff3370ca4bd5ba5f9e727dbb198e8.shib001 -p x --threads=36
